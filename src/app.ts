ca3535a7fdca5ff37754934f549dcbd16c1c28f4
Dan Ruxton <dan@tantalus-troupe.com>
2023-02-19 20:33:38 +0000

    buildspec and app changes

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,21 @@ export const lambdaHandler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "hello world",
-    }),
-  };
+  if (event.httpMethod === "GET") {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "GET REQUEST SUCCESSFUL",
+      }),
+    };
+  } else {
+    return {
+      statusCode: 555,
+      body: JSON.stringify({
+        message: "Method doesn't exist",
+      }),
+    };
+  }
 };
